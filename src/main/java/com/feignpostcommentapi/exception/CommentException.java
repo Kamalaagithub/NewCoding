56b563d25063c7baffd5d4d983ff15d8dda8f715
Kamalaagithub <119840013+Kamalaagithub@users.noreply.github.com>
2023-05-03 12:20:48 +0530

    Update CommentException.java

--- src/main/java/com/feignpostcommentapi/exception/CommentException.java
+++ src/main/java/com/feignpostcommentapi/exception/CommentException.java
@@ -1,1 +1,22 @@
+package com.feignpostcommentapi.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CommentException extends RuntimeException
+{
+    
+	private static final long serialVersionUId = 1L;
+
+	public CommentException(String message) {
+		super(message);
+		
+	}
+	
+	public CommentException(String message,Throwable cause) {
+		super(message, cause);
+		
+	}
+	
+}
