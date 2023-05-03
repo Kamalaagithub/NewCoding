b3b7543936265027c05a83d0662f11099076ee84
Kamalaagithub <119840013+Kamalaagithub@users.noreply.github.com>
2023-05-03 14:19:38 +0530

    Update FileUploadController.java

--- src/main/java/com/fileUploadDownload/controller/FileUploadController.java
+++ src/main/java/com/fileUploadDownload/controller/FileUploadController.java
@@ -3,7 +3,6 @@ package com.fileUploadDownload.controller;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.fileUploadDownload.entity.DatabaseFile;
 import com.fileUploadDownload.payload.FileUploadResponse;
 import com.fileUploadDownload.service.FileStorageService;
@@ -20,16 +18,20 @@ import com.fileUploadDownload.service.FileStorageService;
 @RequestMapping("/uploadfile")
 public class FileUploadController
 {
-	@Autowired
 	private FileStorageService fileStorageService;
+	
+	@Autowired
+	public FileUploadController(FileStorageService thefileStorageService)
+	{
+		this.fileStorageService = thefileStorageService;
+	}
 	
 	@PostMapping("/uploadSinglefile")
     public FileUploadResponse uploadSingleFile(@RequestParam("file") MultipartFile file)
     {
    	 DatabaseFile fileName = fileStorageService.storeFile(file);
-   	 
-   	 
-   	 ///http://localhost:8082/download/abc.pdf
+     	 
+   	 //http://localhost:8082/download/abc.pdf
    	 String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
    			 .path("/downloadfile/")
    			 .path(fileName.getFileName())
